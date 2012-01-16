eefb7ff95b77014b2d2f1734e0c451c3dab970dd
Joshua Buysse <buysse@umn.edu>
2012-01-16 15:40:49 -0600

    Small fixes.

--- cookbooks/class_php/metadata.rb
+++ cookbooks/class_php/metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "CLASS-OAD"
 maintainer_email  "classoad@umn.edu"
 description       "Installs and maintains php and php modules"
-version           "0.1"
+version           "0.1.1"
 
 depends "php"
