aa28cd48dbebea3187dc8cb9dc893560bb17860f
Eric Perrino <perr0273@umn.edu>
2012-01-18 17:37:14 -0600

    class_gituser: merging changes from CLASS repo

--- cookbooks/class_gituser/recipes/default.rb
+++ cookbooks/class_gituser/recipes/default.rb
@@ -1,70 +1,98 @@
 #include_recipe "git"
 
 # add the git user
-user node['gituser']['user'] do
-  home node['gituser']['home_dir']
+user node['class_gituser']['user'] do
+  home node['class_gituser']['home_dir']
   comment "git user"
   shell "/bin/bash"
   system true
   supports :manage_home => true
 end
 
-directory "#{node['gituser']['home_dir']}/packages" do
-  owner node['gituser']['user']
-  group node['gituser']['group']
+directory "#{node['class_gituser']['home_dir']}/packages" do
+  owner node['class_gituser']['user']
+  group node['class_gituser']['group']
   mode "0700"
 end
 
 %w{ git_home git_scripts minion-git-userdir-live }.each do |name|
-  cookbook_file "#{node['gituser']['home_dir']}/packages/#{name}.tar.gz" do
+  cookbook_file "#{node['class_gituser']['home_dir']}/packages/#{name}.tar.gz" do
     source "#{name}.tar.gz"
-    owner node['gituser']['user']
-    group node['gituser']['group']
+    owner node['class_gituser']['user']
+    group node['class_gituser']['group']
     mode "0600"
-    not_if { File.exists?("#{node['gituser']['home_dir']}/packages/#{name}.tar.gz") }
+    not_if { File.exists?("#{node['class_gituser']['home_dir']}/packages/#{name}.tar.gz") }
   end
 end
 
 # create the git home directory
-execute "tar zxvf #{node['gituser']['home_dir']}/packages/git_home.tar.gz" do
-  cwd node['gituser']['home_dir']
-  user node['gituser']['user']
-  group node['gituser']['group']
-  not_if { File.exists?("#{node['gituser']['home_dir']}/gitserver") }
-end
-
-# create the git repository
-directory node['gituser']['gitroot_dir'] do
-  owner node['gituser']['user']
-  group node['gituser']['group']
-  mode "0700"
-  recursive true
-end
-
-execute "tar zxvf #{node['gituser']['home_dir']}/packages/minion-git-userdir-live.tar.gz" do
-  cwd node['gituser']['gitroot_dir']
-  user node['gituser']['user']
-  group node['gituser']['group']
-  not_if { File.directory?("#{node['gituser']['gitroot_dir']}/minion-git-userdir-live.git") }
+execute "tar zxvf #{node['class_gituser']['home_dir']}/packages/git_home.tar.gz" do
+  cwd node['class_gituser']['home_dir']
+  user node['class_gituser']['user']
+  group node['class_gituser']['group']
+  not_if { File.exists?("#{node['class_gituser']['home_dir']}/gitserver") }
 end
 
 # create git scripts
-directory node['gituser']['scripts_dir'] do
+directory node['class_gituser']['scripts_dir'] do
   mode "0755"
   recursive true
 end
 
-execute "tar zxvf #{node['gituser']['home_dir']}/packages/git_scripts.tar.gz" do
-  cwd node['gituser']['scripts_dir']
-  not_if { File.exists?("#{node['gituser']['scripts_dir']}/git-sign") }
+# extract the scripts from a tarball (skip if scripts already present)
+execute "tar zxvf #{node['class_gituser']['home_dir']}/packages/git_scripts.tar.gz" do
+  cwd node['class_gituser']['scripts_dir']
+  not_if { File.exists?("#{node['class_gituser']['scripts_dir']}/git-sign") }
 end
 
+# ensure correct permissions
 %w{ git-create-repo git-sign git-transfer git-verify-tagger }.each do |script|
-  file "#{node['gituser']['scripts_dir']}/#{script}" do
+  file "#{node['class_gituser']['scripts_dir']}/#{script}" do
     mode "0755"
   end
+end
 
-  link "/usr/local/bin/#{script}" do
-    to "#{node['gituser']['scripts_dir']}/#{script}"
+# add scripts directory to the user path
+%w{ sh csh }.each do |ext|
+  template "/etc/profile.d/git-scripts.#{ext}" do
+    source "git-scripts.#{ext}.erb"
+    owner "root"
+    group "root"
+    mode "0644"
   end
+end
+
+# create the git origin directory (if path specified)
+if not node['class_gituser']['origin_dir'].empty?
+  directory node['class_gituser']['origin_dir'] do
+    owner node['class_gituser']['user']
+    group node['class_gituser']['group']
+    mode "0755"
+    recursive true
+  end
+
+  execute "tar zxvf #{node['class_gituser']['home_dir']}/packages/minion-git-userdir-live.tar.gz" do
+    cwd node['class_gituser']['origin_dir']
+    user node['class_gituser']['user']
+    group node['class_gituser']['group']
+    not_if { File.directory?("#{node['class_gituser']['origin_dir']}/minion-git-userdir-live.git") }
+  end
+end
+
+# create the git test directory (if path specified)
+if not node['class_gituser']['test_dir'].empty?
+  directory node['class_gituser']['test_dir'] do
+    owner node['class_gituser']['user']
+    group node['class_gituser']['group']
+    mode "0755"
+    recursive true
+  end
+end
+
+# create the git live directory
+directory node['class_gituser']['live_dir'] do
+  owner node['class_gituser']['user']
+  group node['class_gituser']['group']
+  mode "0755"
+  recursive true
 end
