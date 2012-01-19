8b2e204579d667af075f13908e5afdbea854414b
Joshua Buysse <buysse@umn.edu>
2012-01-19 00:33:54 -0600

    Basic web roles started.

---
+++ cookbooks/cla_unix_baseline/recipes/web_rails_packages.rb
@@ -0,0 +1,43 @@
+#
+# Cookbook Name:: cla_unix_baseline
+# Recipe:: web_rails_packages
+#
+# Copyright 2011, Joshua Buysse, (C) Regents of the University of Minnesota
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+include_recipe "cla_unix_baseline::web_packages"
+
+ubuntu_lucid_plist = %w( ruby1.8 ruby1.8-dev rubygems ruby-dev libopenssl-ruby rdoc 
+)
+
+# STUB:
+rh_5_plist = %w( 
+)
+
+gem_list = %w( mysql passenger rails capistrano rcov ) 
+
+case node[:platform]
+when "ubuntu"
+  ubuntu_lucid_plist.each do |pkg|
+    package pkg
+  end
+when "redhat", "centos" 
+  rh_5_plist.each do |pkg|
+    package pkg
+  end
+end
+
+gem_list.each do |gem|
+  gem_package gem
+end
