e2faac7cba9d03abdcb2a4bb45aef3b64aa25599
Eric Perrino <perr0273@umn.edu>
2012-01-27 14:29:32 -0600

    cla_sudo: fixed issues with cla_sudo_commands definition

--- cookbooks/cla_sudo/definitions/default.rb
+++ cookbooks/cla_sudo/definitions/default.rb
@@ -1,5 +1,6 @@
 define :cla_sudo_commands, :commands => [], :allowed_group => "sysadmin", :target_user => "root" do 
   template "/etc/sudoers.d/cla_sudo_#{params[:name]}" do 
+    cookbook "cla_sudo"
     source "cla_sudo_commands.erb"
     owner "root"
     group "root"
