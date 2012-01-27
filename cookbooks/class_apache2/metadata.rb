78d5c22a0ebb6c6e5d321ab45a51ca8a627ac222
Eric Perrino <perr0273@umn.edu>
2012-01-27 14:56:28 -0600

    incrementing version numbers for class_apache2 and class_gituser

--- cookbooks/class_apache2/metadata.rb
+++ cookbooks/class_apache2/metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "CLASS-OAD"
 maintainer_email  "classoad@umn.edu"
 description       "Additional CLASS-specific configuration for apache"
-version           "0.1.1"
+version           "0.1.2"
 
 depends "apache2"
