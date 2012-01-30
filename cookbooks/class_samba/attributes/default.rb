76378079f0f15690ca097196f97cfdc73c3c63da
Eric Perrino <perr0273@umn.edu>
2012-01-30 17:01:19 -0600

    class_samba: added socket, name resolution, ntlmv2, and master options to config removed options where we were already using the default disable unneeded nmbd service

--- cookbooks/class_samba/attributes/default.rb
+++ cookbooks/class_samba/attributes/default.rb
@@ -7,7 +7,6 @@ default["class_samba"]["load_printers"] = "no"
 default["class_samba"]["passdb_backend"] = "tdbsam"
 default["class_samba"]["dns_proxy"] = "no"
 default["class_samba"]["security"] = "user"
-default["class_samba"]["map_to_guest"] = "Bad User"
 default["class_samba"]["socket_options"] = "TCP_NODELAY"
 
 case platform
@@ -22,12 +21,38 @@ else
   set["class_samba"]["log_dir"] = "/var/log/samba/%m.log"
 end
 
-# added by CLASS-OAD
-default["class_samba"]["local_master"] = "no"
-default["class_samba"]["unix_password_sync"] = "yes"
-default["class_samba"]["pam_password_change"] = "yes"
-default["class_samba"]["wide_links"] = "no"
+#
+# CLASS-OAD additions
+#
+
+# socket buffer optimizations
+default["class_samba"]["socket_options"] = "TCP_NODELAY SO_RCVBUF=8192 SO_SNDBUF=8192"
+
+# require NTLMv2 authentication
+default["class_samba"]["client_ntlmv2_auth"] = "yes"
+
+# require NT protocol
+default["class_samba"]["min_protocol"] = "NT1"
+
+# prevent enumeration of shares to anonymous users
 default["class_samba"]["restrict_anonymous"] = 2
+
+# standard hostname resolution
+default["class_samba"]["name_resolve_order"] = "host"
+
+# disable printing
 default["class_samba"]["printing"] = "bsd"
 default["class_samba"]["printcap_name"] = "/dev/null"
 default["class_samba"]["disable_spoolss"] = "yes"
+
+# don't try to become a master server
+default["class_samba"]["domain_master"] = "no"
+default["class_samba"]["local_master"] = "no"
+default["class_samba"]["preferred_master"] = "no"
+
+# disable lanman broadcasts
+default["class_samba"]["lm_announce"] = "no"
+
+# disable netbios broadcasts (be sure to disable nmbd service)
+default["class_samba"]["disable_netbios"] = "yes"
+
