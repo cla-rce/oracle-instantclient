48f76383a04fba35ccc6d8096bd4b6e778de1568
Joshua Buysse <buysse@umn.edu>
2012-02-02 09:50:06 -0600

    Just filled in the file for dummy cert generation

--- config/rake.rb
+++ config/rake.rb
@@ -6,22 +6,22 @@
 ###
 
 # The company name - used for SSL certificates, and in srvious other places
-COMPANY_NAME = "Example Com"
+COMPANY_NAME = "University of Minnesota"
 
 # The Country Name to use for SSL Certificates
 SSL_COUNTRY_NAME = "US"
 
 # The State Name to use for SSL Certificates
-SSL_STATE_NAME = "Several"
+SSL_STATE_NAME = "Minnesota"
 
 # The Locality Name for SSL - typically, the city
-SSL_LOCALITY_NAME = "Locality"
+SSL_LOCALITY_NAME = "Minneapolis"
 
 # What department?
-SSL_ORGANIZATIONAL_UNIT_NAME = "Operations"
+SSL_ORGANIZATIONAL_UNIT_NAME = "College of Liberal Arts"
 
 # The SSL contact email address
-SSL_EMAIL_ADDRESS = "ops@example.com"
+SSL_EMAIL_ADDRESS = "servers@rt.cla.umn.edu"
 
 # License for new Cookbooks
 # Can be :apachev2 or :none
