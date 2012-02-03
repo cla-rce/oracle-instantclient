a99a6c80672eba34175cfe2e6b3210dbd465d9f1
Eric Perrino <perr0273@umn.edu>
2012-02-03 16:54:10 -0600

    class_dbrep: changed path in dbrep_server to point to staging instance

--- cookbooks/class_dbrep/metadata.rb
+++ cookbooks/class_dbrep/metadata.rb
@@ -1,4 +1,4 @@
 maintainer        "CLASS-OAD"
 maintainer_email  "classoad@umn.edu"
 description       "Installs the dbrep user and preps the application directory."
-version           "0.1.3"
+version           "0.1.4"
