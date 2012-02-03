8cf7acb76c5db4ef800bf123460ca66580ba7dea
Eric Perrino <perr0273@umn.edu>
2012-02-02 18:44:14 -0600

    class_clustersync: fixed rsync command check in validate-push

--- cookbooks/class_clustersync/metadata.rb
+++ cookbooks/class_clustersync/metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "CLASS-OAD"
 maintainer_email  "classoad@umn.edu"
 description       "Installs data replication scripts for web cluster."
-version           "0.3.0"
+version           "0.3.1"
 
 depends "logrotate_37signals"
