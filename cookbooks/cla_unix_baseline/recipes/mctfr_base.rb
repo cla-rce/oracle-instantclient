78901bdd3361fba5f8225f1a199e4b67b0dfb4b2
Joshua Buysse <buysse@umn.edu>
2012-02-06 23:23:37 -0600

    Added the baseline for MCTFR integration.  At this time, just does the automounter work.  Assumes that AD integration is already done, via RID-based UIDs (nfsv4 should keep that sane). AD join has to be done manually

---
+++ cookbooks/cla_unix_baseline/recipes/mctfr_base.rb
@@ -0,0 +1,54 @@
+#
+# Cookbook Name:: cla_unix_baseline
+# Recipe:: mctfr_base
+#
+# Copyright 2012, Joshua Buysse, (C) Regents of the University of Minnesota
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Need to set up the automounter on the host.
+
+ubuntu_lucid_plist = %w( autofs5 )
+
+rh_5_plist = %w( autofs )
+
+case node[:platform]
+when "ubuntu"
+  ubuntu_lucid_plist.each do |pkg|
+    package pkg
+  end
+  
+when "redhat", "centos" 
+  rh_5_plist.each do |pkg|
+    package pkg
+  end
+
+end
+
+# install automounter configuration
+cookbook_file "/etc/auto.master" do 
+  source "auto.master.mctfr"
+end
+
+cookbook_file "/etc/auto.home" do 
+  source "auto.home.mctfr"
+end
+
+cookbook_file "/etc/auto.shared" do 
+  source "auto.shared.mctfr"
+end
+
+service "autofs" do 
+  action [:enable, :start]
+end
