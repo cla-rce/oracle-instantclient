81e8e350b09c8484f05be86ba6e59f291de10010
Joshua Buysse <buysse@umn.edu>
2012-02-06 22:21:44 -0600

    Added ability to exclude directories from clamav, and a maximum filesize to scan

--- cookbooks/cla_clamav/attributes/default.rb
+++ cookbooks/cla_clamav/attributes/default.rb
@@ -1,3 +1,7 @@
 default[:cla_clamav][:email_to] = "servers@rt.cla.umn.edu"
 # valid options are daily, weekly, monthly, quarterly, yearly
 default[:cla_clamav][:run_frequency] = "weekly"
+# array of excluded paths on local host
+default[:cla_clamav][:exclude_dirs] = []
+# ignore larger files for speed
+default[:cla_clamav][:max_filesize] = "1M"
