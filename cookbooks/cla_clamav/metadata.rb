7ff1974c7e84e82e135f6ba088c226d14a83941d
Joshua Buysse <buysse@umn.edu>
2012-02-06 21:19:43 -0600

    Fully working, perl bugs fixed.

--- cookbooks/cla_clamav/metadata.rb
+++ cookbooks/cla_clamav/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "CLASS-OAD"
 maintainer_email  "classoad@umn.edu"
 description       "Installs the ClamAV virus scanner."
-version           "0.2.2"
+version           "0.2.3"
 
 depends "yum"
 depends "perl"
