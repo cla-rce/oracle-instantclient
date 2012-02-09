d4251e7279eb349c32deb172768623ea1b71e2d4
Eric Perrino <perr0273@umn.edu>
2012-02-09 17:45:25 -0600

    class_php: only create php.ini files for installed sapis, added cgi sapi config

--- cookbooks/class_php/metadata.rb
+++ cookbooks/class_php/metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "CLASS-OAD"
 maintainer_email  "classoad@umn.edu"
 description       "Installs and maintains php and php modules"
-version           "0.1.2"
+version           "0.1.3"
 
 depends "php"
