6067b4cb89cc2e73a924a148b478544ae1dcee09
Joshua Buysse <buysse@umn.edu>
2012-02-11 20:25:19 -0600

    Fixed some damage done by cla_auth integration changes and frozen cla_auth in prod.

--- cookbooks/cla_auth/metadata.rb
+++ cookbooks/cla_auth/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "buysse@umn.edu"
 license          "Apache 2.0"
 description      "Installs/Configures cla_auth"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.0"
+version          "0.6.1"
 
 %w{redhat centos ubuntu }.each do |os|
   supports os
