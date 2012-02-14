687e6f006c13f2b62e5d1edf20c2750ef3e40281
Joshua Buysse <buysse@gmail.com>
2012-02-14 10:58:33 -0600

    Added support for nice value in cla_clamav wrapper

--- cookbooks/cla_clamav/metadata.rb
+++ cookbooks/cla_clamav/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "CLASS-OAD"
 maintainer_email  "classoad@umn.edu"
 description       "Installs the ClamAV virus scanner."
-version           "0.3.2"
+version           "0.4.0"
 
 depends "yum"
 depends "perl"
