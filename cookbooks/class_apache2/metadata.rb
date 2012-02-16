4ab180238f6a25cd7572f2fad382ade38c392ace
Eric Perrino <perr0273@umn.edu>
2012-02-16 15:56:39 -0600

    class_apache2: changed LogFormat nickname to combinedVhost to avoid conflicting with apache default

--- cookbooks/class_apache2/metadata.rb
+++ cookbooks/class_apache2/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "CLASS-OAD"
 maintainer_email  "classoad@umn.edu"
 description       "Additional CLASS-specific configuration for apache"
-version           "0.1.4"
+version           "0.1.5"
 
 depends "apache2"
 depends "logrotate_37signals"
