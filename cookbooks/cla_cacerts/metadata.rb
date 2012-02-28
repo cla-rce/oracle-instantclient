8b158468fc06b52f628904c2428518881c67c459
Joshua Buysse <buysse@umn.edu>
2012-02-27 21:02:33 -0600

    Added our old CA cert in to the bundle.

--- cookbooks/cla_cacerts/metadata.rb
+++ cookbooks/cla_cacerts/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "buysse@umn.edu"
 license          "Apache 2.0"
 description      "Installs/Configures cla_cacerts"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 supports "redhat", ">= 5.0"
 supports "centos", ">= 5.0"
