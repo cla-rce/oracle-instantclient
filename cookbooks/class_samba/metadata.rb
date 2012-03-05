ef57048f84f8e9b216fda3ad19923998420b47dc
Joshua Buysse <buysse@gmail.com>
2012-03-05 14:49:27 -0600

    Added logging, fixed paths for binaries, and some error-state checking on the sambapasswd script from Eric.

--- cookbooks/class_samba/metadata.rb
+++ cookbooks/class_samba/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "CLASS-OAD"
 maintainer_email  "classoad@umn.edu"
 description       "Additional CLASS-specific configuration for samba"
-version           "0.1.3"
+version           "0.1.4"
 
 depends "samba"
 depends "cla_sudo"
