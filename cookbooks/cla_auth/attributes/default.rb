5fdd0d2a54413231773254504c7d48cc9497fdd6
Joshua Buysse <buysse@gmail.com>
2012-06-26 12:05:33 -0500

    Added default domain attribute (missed file in last commit.

--- cookbooks/cla_auth/attributes/default.rb
+++ cookbooks/cla_auth/attributes/default.rb
@@ -2,6 +2,8 @@ default[:cla_auth][:ldap_base] = "ou=People,o=University of Minnesota,c=US"
 default[:cla_auth][:ldap_servers] = [ "ldaps://ldapauth.umn.edu" ]
 default[:cla_auth][:mkey_radius_secret] = "Wb!gy81qern9xe#2"
 default[:cla_auth][:mkey_radius_hosts] = %w{ 192.168.110.225:1645 192.168.110.226:1645 192.168.110.227:1645 }
+default[:cla_auth][:nfs4_domain_override] = "localdomain"
+
 case platform
 when "ubuntu"
   default[:cla_auth][:ldap_cacert_fname] = "ca-certificates.crt"
