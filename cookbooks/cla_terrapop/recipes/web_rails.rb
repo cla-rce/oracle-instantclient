d5f027f9b8a7d540f31bdadc0fc52de16e5eb457
Joshua Buysse <buysse@gmail.com>
2012-07-02 11:35:03 -0500

    First run at configuring vhosts for terrapop in a recipe.

--- cookbooks/cla_terrapop/recipes/web_rails.rb
+++ cookbooks/cla_terrapop/recipes/web_rails.rb
@@ -44,3 +44,90 @@ end
 
 # configure passenger for Apache
 # stub
+
+# set up vhosts
+# for the moment, hard code.  Move to data bag after working.
+include_recipe "apache2::mod_ssl"
+
+# use lwrp from apache2 cookbook
+vhosts = ['demo.data.terrapop.org', 'dev.data.terrapop.org', 'staging.data.terrapop.org']
+
+vhosts.each do |vh|
+  # ensure that docroot is there
+  ### STUB: assuming group terrapop already exists
+  directory "/web/#{vh}" do 
+    owner "root"
+    group "terrapop"
+    mode "02775"
+  end
+  
+  directory "/web/#{vh}/htdocs" do 
+    owner "root"
+    group "terrapop"
+    mode "02775"
+  end
+  
+  directory "/web/#{vh}/rails" do 
+    owner "root"
+    group "terrapop"
+    mode "02775"
+  end
+  
+  # set up the cert directories and ensure that they're there
+  # STUB: ubuntu configuration; may not work on other os due to missing group
+  directory "/etc/ssl" do
+    owner "root"
+    group "root"
+    mode "0755"
+  end
+  directory "/etc/ssl/certs" do 
+    owner "root"
+    group "root"
+    mode "0755"
+  end
+  directory "/etc/ssl/private" do 
+    owner "root"
+    group "ssl-cert"
+    mode "0710"
+  end
+
+  file "/etc/ssl/certs/#{vh}.crt" do 
+    source "#{vh}.crt"
+    owner "root"
+    group "root"
+    mode "0644"
+  end
+  
+#  file "/etc/ssl/certs/#{vh}_intermediate.crt" do 
+#    source "#{vh}_intermediate.crt"
+#    owner "root"
+#    group "root"
+#    mode "0644"
+#  end
+  
+  file "/etc/ssl/private/#{vh}.key" do 
+    source "#{vh}.key"
+    owner "root"
+    group "root"
+    mode "0600"
+  end
+
+  # actually configure vhosts.
+  # overriding the template to add SSL support to the apache2 recipe
+  ## STUB: contribute code back to opscode on apache2 cookbook
+  web_app "#{vh}" do 
+    server_name vh
+    docroot "/web/#{vh}/htdocs"
+    template "rails_web_app.conf.erb"
+  end
+
+  web_app "#{vh}_ssl" do 
+    server_name vh
+    docroot "/web/#{vh}/htdocs"
+    template "rails_web_app.conf.erb"
+    ssl_enabled true
+    ssl_certificate_file "/etc/ssl/certs/#{vh}.crt"
+    ssl_certificate_key_file "/etc/ssl/private/#{vh}.key"
+#    ssl_certificate_chain_file "/etc/ssl/certs/#{vh}_interm.crt"
+  end
+end
