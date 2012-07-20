023e22d187a6b6a409bea963682beee6fa7a16e9
Adam Mielke <adam@umn.edu>
2012-07-20 12:38:17 -0500

    Changed windows attributes on chef-client cookbook in attempt to fix bug

--- cookbooks/chef-client/attributes/default.rb
+++ cookbooks/chef-client/attributes/default.rb
@@ -52,11 +52,11 @@ when "openindiana","opensolaris","nexentacore","solaris2"
   default["chef_client"]["backup_path"] = "/var/chef/backup"
 when "windows"
   default["chef_client"]["init_style"]  = "winsw"
-  default["chef_client"]["run_path"]    = "C:/var/run/chef"
-  default["chef_client"]["cache_path"]  = "C:/var/chef/cache"
-  default["chef_client"]["backup_path"] = "C:/var/chef/backup"
+  default["chef_client"]["run_path"]    = "C:/chef"
+  default["chef_client"]["cache_path"]  = "C:/chef/cache"
+  default["chef_client"]["backup_path"] = "C:/chef/backup"
   default["chef_client"]["conf_dir"]    = "C:/chef"
-  default["chef_client"]["bin"]         = File.join(Gem.bindir, "chef-client")
+  default["chef_client"]["bin"]         = "C:/opscode/chef/bin/chef-client.bat"
   #Required for minsw wrapper
   default["chef_client"]["ruby_bin"]    = File.join(RbConfig::CONFIG['bindir'], "ruby.exe")
   default["chef_client"]["winsw_url"]   = "http://maven.dyndns.org/2/com/sun/winsw/winsw/1.8/winsw-1.8-bin.exe"
