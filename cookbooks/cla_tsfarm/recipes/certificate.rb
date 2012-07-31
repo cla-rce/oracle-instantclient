e376ecf29b318bedd1cd5e6458829b9c2a7a0192
Adam Mielke <adam@umn.edu>
2012-07-31 10:44:43 -0500

    Simplified/refactored certificate recipe for tsfarm

--- cookbooks/cla_tsfarm/recipes/certificate.rb
+++ cookbooks/cla_tsfarm/recipes/certificate.rb
@@ -17,112 +17,18 @@
 # limitations under the License.
 #
 
-powershell "import certificate" do
+execute "Import certificate" do
+  command "certutil -p \"testing\" -importpfx \"#{node['cla_tsfarm']['cert_location']}\""
+  not_if { Registry.key_exists?("HKLM\\SOFTWARE\\Microsoft\\SystemCertificates\\my\\Certificates\\#{node['cla_tsfarm']['cert_thumbprint']}") }
+  action :run
+end
+
+powershell "Set RDS certificate" do
   code <<-EOH
-#requires -Version 2.0
-
-function Import-Certificate
-{
-	param
-	(
-		[IO.FileInfo] $CertFile = $(throw "Paramerter -CertFile [System.IO.FileInfo] is required."),
-		[string[]] $StoreNames = $(throw "Paramerter -StoreNames [System.String] is required."),
-		[switch] $LocalMachine,
-		[switch] $CurrentUser,
-		[string] $CertPassword,
-		[switch] $Verbose
-	)
-	
-	begin
-	{
-		[void][System.Reflection.Assembly]::LoadWithPartialName("System.Security")
-	}
-	
-	process 
-	{
-        if ($Verbose)
-		{
-            $VerbosePreference = 'Continue'
-        }
-    
-		if (-not $LocalMachine -and -not $CurrentUser)
-		{
-			Write-Warning "One or both of the following parameters are required: '-LocalMachine' '-CurrentUser'. Skipping certificate '$CertFile'."
-		}
-
-		try
-		{
-			if ($_)
-            {
-                $certfile = $_
-            }
-            $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $certfile,$CertPassword
-		}
-		catch
-		{
-			Write-Error ("Error importing '$certfile': $_ .") -ErrorAction:Continue
-		}
-			
-		if ($cert -and $LocalMachine)
-		{
-			$StoreScope = "LocalMachine"
-			$StoreNames | ForEach-Object {
-				$StoreName = $_
-				if (Test-Path "cert:\$StoreScope\$StoreName")
-				{
-					try
-					{
-						$store = New-Object System.Security.Cryptography.X509Certificates.X509Store $StoreName, $StoreScope
-						$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
-						$store.Add($cert)
-						$store.Close()
-						Write-Verbose "Successfully added '$certfile' to 'cert:\$StoreScope\$StoreName'."
-					}
-					catch
-					{
-						Write-Error ("Error adding '$certfile' to 'cert:\$StoreScope\$StoreName': $_ .") -ErrorAction:Continue
-					}
-				}
-				else
-				{
-					Write-Warning "Certificate store '$StoreName' does not exist. Skipping..."
-				}
-			}
-		}
-		
-		if ($cert -and $CurrentUser)
-		{
-			$StoreScope = "CurrentUser"
-			$StoreNames | ForEach-Object {
-				$StoreName = $_
-				if (Test-Path "cert:\$StoreScope\$StoreName")
-				{
-					try
-					{
-						$store = New-Object System.Security.Cryptography.X509Certificates.X509Store $StoreName, $StoreScope
-						$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
-						$store.Add($cert)
-						$store.Close()
-						Write-Verbose "Successfully added '$certfile' to 'cert:\$StoreScope\$StoreName'."
-					}
-					catch
-					{
-						Write-Error ("Error adding '$certfile' to 'cert:\$StoreScope\$StoreName': $_ .") -ErrorAction:Continue
-					}
-				}
-				else
-				{
-					Write-Warning "Certificate store '$StoreName' does not exist. Skipping..."
-				}
-			}
-		}
-	}
-	
-	end
-	{ }
-}
-  Import-Certificate -Certfile #{node['cla_tsfarm']['cert_location']} -StoreNames My -LocalMachine
-  ls cert:/LocalMachine/My
+  Import-Module RemoteDesktopServices
+  $CurrentCert = (Get-Item RDS:/RDSConfiguration/Connections/RDP-Tcp/SecuritySettings/SSLCertificateSHA1Hash).CurrentValue
+  if ($CurrentCert -ne "#{node['cla_tsfarm']['cert_thumbprint']}") {
+    Set-Item RDS:/RDSConfiguration/Connections/RDP-Tcp/SecuritySettings/SSLCertificateSHA1Hash #{node['cla_tsfarm']['cert_thumbprint']}
+  }
   EOH
-  not_if "exit /b 1"
 end
