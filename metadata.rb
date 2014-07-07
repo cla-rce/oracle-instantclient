0faba2f12e2574334ad6d0769e4535207d2917ef
Peter Walz <pnw@umn.edu>
2014-07-07 14:38:51 -0500

    Update version number to reflect changes in recent pull request

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "buysse@umn.edu"
 license          "Apache 2.0"
 description      "Installs/Configures oracle-instantclient"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.3.1"
+version          "0.3.2"
 
 supports "ubuntu"
