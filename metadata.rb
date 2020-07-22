fcdc0cb5a0c584d04f64016bc95de3f76fa7a3d0
Peter Walz <pnw@umn.edu>
2020-07-21 20:58:43 -0500

    Bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "buysse@umn.edu"
 license          "Apache 2.0"
 description      "Installs/Configures oracle-instantclient"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.5.0"
+version          "0.5.1"
 
 supports "centos", ">= 6.0"
 supports "redhat", ">= 6.0"
